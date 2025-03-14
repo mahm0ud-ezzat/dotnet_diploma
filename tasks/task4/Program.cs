d17bb66d39f14c42145d9679d3d27fd27607f9d2
unknown <mahmoud ezzat>
2025-03-14 17:04:19 +0200

    creating first object in c#

---
+++ tasks/task4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace task4
+{
+    class Animal
+    {
+        int age;
+        string name;
+        string type;
+        bool isMale;
+        public Animal(int age, string name, string type, bool isMale)
+        {
+            this.age = age;
+            this.name = name;
+            this.type = type;
+            this.isMale = isMale;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal(13, "rock", "dog", true);
+            Animal animal2 = new Animal(5, "rocka", "dog", false);
+
+        }
+    }
+}
